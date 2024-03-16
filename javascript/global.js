31378344f09cf49c1b624d64ced45bb8164a313f
Tilo Krohn <67702270+tilokrohn@users.noreply.github.com>
2024-03-16 14:25:55 +0100

    Add files via upload

---
+++ javascript/global.js
@@ -0,0 +1,9 @@
+// Auslösen der KLICK-Funktion im Hamburger-Symbol
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
